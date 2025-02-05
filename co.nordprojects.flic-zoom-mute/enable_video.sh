018bc667a1fcdbf0267e08b38571488bc5959cdc
Joe Rickerby <joerick@mac.com>
2025-02-05 13:50:54 +0000

    Added more function

---
+++ co.nordprojects.flic-zoom-mute/enable_video.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+osascript -e '
+    if application "zoom.us" is running then
+            tell application "System Events" to tell process "zoom.us"
+                    if menu item "Start video" of menu 1 of menu bar item "Meeting" of menu bar 1 exists then
+                            click menu item "Start video" of menu 1 of menu bar item "Meeting" of menu bar 1
+                    end if
+            end tell
+    end if
+'
